311996aca6903b1fefeae2dd1234740d131e057e
paul wille <paul.wille@t-online.de>
2015-12-09 14:28:06 +0100

    new test init

--- test/tests.py
+++ test/tests.py
@@ -3,40 +3,57 @@ currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfram
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0,parentdir)
 
-# os.environ["COOKIE_SECRET"] = "secret"
-# os.environ["GITHUB_CLIENT_ID"] = "x"
-# os.environ["GITHUB_SECRET"] = "y"
-# os.environ["DATABASE_URL"] = "mysql://Oleg1@localhost/db1"
-
+from peewee import *
 from database import MDB as Database
 
-from config import dbconf, bsconf
+from config import settings, dbconf, bsconf
 from models import *
+
 import models
 
-from peewee import *
-from database import *
-from peewee import fn
-
-
-import tornado.testing
-import unittest
-
 database = Database(**dbconf)
-dbproxy = Proxy()
-dbproxy.initialize(database)
+blobstore = None # Blobstore(bsconf.nodes, **bsconf.opts)
+models.initialize(database, blobstore)
 
 
-# blobstore = None # Blobstore(bsconf.nodes, **bsconf.opts)
-# models.initialize(database, blobstore)
-# database.create_tables([
-#     User,
-#     Token,
-#     Repo,
-#     HMap,
-#     CSet,
-#     Blob,
-# ])
+
+
+# # os.environ["COOKIE_SECRET"] = "secret"
+# # os.environ["GITHUB_CLIENT_ID"] = "x"
+# # os.environ["GITHUB_SECRET"] = "y"
+# os.environ["DATABASE_URL"] = "mysql://Oleg1@localhost/db1"
+
+# from database import MDB as Database
+
+# # from config import dbconf, bsconf
+# from models import *
+# import models
+
+# from peewee import *
+# from database import *
+# from peewee import fn
+# from playhouse.db_url import connect
+
+
+# import tornado.testing
+# import unittest
+
+# # database = Database(**dbconf)
+# # dbproxy = Proxy()
+# # dbproxy.initialize(database)
+
+# databse = connect("mysql://tailr:tailr@db/tailr")
+
+# # blobstore = None # Blobstore(bsconf.nodes, **bsconf.opts)
+# # models.initialize(database, blobstore)
+# # database.create_tables([
+# #     User,
+# #     Token,
+# #     Repo,
+# #     HMap,
+# #     CSet,
+# #     Blob,
+# # ])
 
 
 
@@ -51,9 +68,10 @@ def multiply(x,y):
 	return x * y
 
 def seed():
-	user1 = User.create(name="user1", email="user1@example.com")
-	user2 = User.create(name="user2", email="user2@example.com")
+	user1 = User.create(name="user4", email="user1@example.com")
+	user2 = User.create(name="user3", email="user2@example.com")
 	repo = Repo.create(user=user1, name="testrepo1", desc="")
+	print ("baba")
 
 seed()
 
@@ -68,24 +86,24 @@ class MyTest(unittest.TestCase):
 		self.assertEqual(multiply(5,6),30)
 
 
-# Create Account with credentials
-# class Create
+# # Create Account with credentials
+# # class Create
 
 
-# Login and check for success
+# # Login and check for success
 
-# Sign in with Github account
+# # Sign in with Github account
 
-# Create repo
+# # Create repo
 
-# Show Repo
+# # Show Repo
 
-# Push valid data and check for success
+# # Push valid data and check for success
 
-#
+# #
 
-# push invalid data and check response
+# # push invalid data and check response
 
 
-if __name__ == '__main__':
-	unittest.main()
+# if __name__ == '__main__':
+# 	unittest.main()
