555693c98ddff74ac9b36f34048d5e9324a1482e
paul wille <paul.wille@t-online.de>
2016-01-27 13:52:23 +0100

    First set of tests

--- test/tests.py
+++ test/tests.py
@@ -16,7 +16,7 @@ blobstore = None # Blobstore(bsconf.nodes, **bsconf.opts)
 models.initialize(database, blobstore)
 
 
-
+# import requests
 
 # # os.environ["COOKIE_SECRET"] = "secret"
 # # os.environ["GITHUB_CLIENT_ID"] = "x"
@@ -36,7 +36,9 @@ models.initialize(database, blobstore)
 
 
 # import tornado.testing
-# import unittest
+import unittest
+import requests
+import json
 
 # # database = Database(**dbconf)
 # # dbproxy = Proxy()
@@ -59,31 +61,181 @@ models.initialize(database, blobstore)
 
 user1 = User()
 user2 = User()
-repo = Repo()
+repo1 = Repo()
+repo2 = Repo()
+token = Token()
 
-def addOne(x):
-	return x + 1
-
-def multiply(x,y):
-	return x * y
 
 def seed():
-	user1 = User.create(name="user4", email="user1@example.com")
-	user2 = User.create(name="user3", email="user2@example.com")
-	repo = Repo.create(user=user1, name="testrepo1", desc="")
-	print ("baba")
+	user1 = User.create(name="user1", confirmed=True, github_id="1234", email="user1@example.com")
+	user2 = User.create(name="user2", confirmed=True, github_id="5678", email="user2@example.com")
+	token = Token.create(value="123456", user=user1, seen=True, desc="important description")
+	repo1 = Repo.create(user=user1, name="repo1", desc="important description")
+	repo2 = Repo.create(user=user2, name="repo2", desc="important description")
 
+# TODO for local testing check if db is freshly initialized, or has already been seeded  
 seed()
 
-class MyTest(unittest.TestCase):
+
+class Authorized(unittest.TestCase):
+
+	#######	#######	#######	#######	#######	#######	#######	#######	#######	#######
+	# unittest module does sort the tests with cmp()
+	# therefore tests are orderer by naming them testXYZ
+	# third digit is there to slot tests in
+	#######	#######	#######	#######	#######	#######	#######	#######	#######	#######
+
 	def setUp(self):
 		pass
 
-	def test_number_five(self):
-		self.assertEqual(addOne(5),6)
+	tailrToken = "123456"
+	userName = "user1"
+	repoName = "repo1"
+	repo = (Repo.select(Repo.id).where((Repo.name == "repo1")).naive().get())
+	
+	key = "http://rdf.data-vocabulary.org/"
+	keyWithFragment = "http://rdf.data-vocabulary.org/#fragment"
 
-	def test_multiply(self):
-		self.assertEqual(multiply(5,6),30)
+	uploadDateString = "2013-07-12-00:00:00"
+	uploadDateString2 = "2014-08-13-00:00:00"
+
+	params_key = {'key':key}
+	params_key_timemap = {'key':key, 'timemap': "true"}
+	params_index = {'index': "true"}
+	params_datetime = {'key':key,'datetime':uploadDateString}
+	params_datetime2 = {'key':key,'datetime':uploadDateString2}
+	empty_params = {}
+
+	contentType = "application/n-triples"
+	header = {'Authorization':"token "+tailrToken, 'Content-Type':contentType}
+	apiURI = "http://localhost:5000/api/"+userName+"/"+repoName
+	apiURI2 = "http://localhost:5000/api/user2/repo2"
+	notExistingRepo = "http://localhost:5000/api/user1/XXX"
+
+	# payload = set()
+	# payload.add("<www.example.com> <http://www.w3.org/2002/07/owl#onProperty> <http://qudt.org/schema/qudt#default> .")
+	payload = "<http://data.bnf.fr/ark:/12148/cb308749370#frbr:Expression> <http://data.bnf.fr/vocabulary/roles/r70> <http://data.bnf.fr/ark:/12148/cb12204024r#foaf:Person> ."
+	payload2 = "<http://data.bnf.fr/ark:/12148/cb308749370#frbr:Expression> <http://data.bnf.fr/vocabulary/roles/r70> <http://data.bnf.fr/ark:/12148/cb12204024r#foaf:Person> ."+"\n"+"<http://data.bnf.fr/ark:/12148/cb308749370#frbr:Expressions> <http://data.bnf.fr/vocabulary/roles/s70> <http://data.bnf.fr/ark:/12148/cb12204024r#foaf:Persons> ."
+
+	# def uploadValid(self):
+	# 	r = request.put(apiURI, params=params, headers=header, data=payload)
+	# 	# r = request.put(apiURI, params=params, headers=header)
+	# 	self.assertEqual(r.status_code, 500)
+
+	@staticmethod
+	def numberOfCSets(repo):
+		cs = CSet.select().where(CSet.repo == repo)
+		# print ("count of changesets for "+repo.name+" = "+cs.count())
+		return cs.count()
+
+
+	def test010_put_on_empty(self):
+		r = requests.put(self.apiURI, params=self.params_datetime, headers=self.header, data=self.payload2)
+		self.assertEqual(r.status_code, 200)
+
+	def test011_number_of_changesets_is_set(self):
+		self.assertEqual(self.numberOfCSets(self.repo),1)
+
+		
+	def test020_put_on_existing(self):
+		r = requests.put(self.apiURI, params=self.params_key, headers=self.header, data=self.payload)
+		self.assertEqual(r.status_code, 200, msg=r.reason)
+
+
+	def test021_number_of_changesets_increased(self):
+		self.assertEqual(self.numberOfCSets(self.repo),2)
+
+
+	def test030_get_repo_index(self):
+		r = requests.get(self.apiURI, params=self.params_index)
+		# only one key in repo
+		self.assertEqual(len(r.text.splitlines()), 1)
+
+
+	def test040_get_repo_timemap(self):
+		r = requests.get(self.apiURI, params=self.params_key_timemap)
+		resjson = json.loads(r.text)
+		# 2 revisions pushed
+		# TODO proper unicode decoding. For some reason, u'' in this json is dealt with as an string
+		self.assertEqual(len(resjson[u'mementos'][u'list']), 2)
+		
+
+	# TODO GET repo ?key mementos on several times (&datetime)
+
+	# TODO GET repo ?key without datetime and timestamp
+	#last memento for key should be shown
+
+
+
+
+	# def test_delete_():
+	# 	pass
+
+	# 
+	# 
+	# 
+	# TODO Also check content of db not only request
+	# 
+	# 
+	# 
+
+
+	def test_put_unowned_repo(self):
+		# unauthorized because of unowned repo
+		# 403 write access fordbidden
+		r = requests.put(self.apiURI2, params=self.params_key, headers=self.header, data=self.payload)
+		self.assertEqual(r.status_code, 403)
+
+	def test_put_without_key(self):
+		# 400 Bad Request
+		r = requests.put(self.apiURI, params=self.empty_params, headers=self.header, data=self.payload)
+		self.assertEqual(r.status_code, 400)
+
+	def test_put_unexisting_repo(self):
+		# 404
+		r = requests.put(self.notExistingRepo, params=self.params_key, headers=self.header, data=self.payload)
+		self.assertEqual(r.status_code, 404)
+
+	# Wird sich in Zukunft eruebrigen, wenn das als feature implementiert ist
+	def test030_put_with_older_timestamp(self):
+		# 400 Bad Request
+		uploadDateString = "2012-07-12-00:00:00"
+		params_datetime = {'key':self.key,'datetime':uploadDateString}
+		r = requests.put(self.apiURI, params=params_datetime, headers=self.header, data=self.payload)
+		self.assertEqual(r.status_code, 400)
+
+
+	# TODO raise 400 three times if (index and timemap) or (index and key) or (timemap and not key):
+            # raise HTTPError(reason="Invalid arguments.", status_code=400)
+
+	# TODO Raise specific errors, if data is compromised
+
+	# TODO raise integrity error somehow
+
+
+class Unauthorized(unittest.TestCase):
+	def setUp(self):
+		pass
+
+	tailrToken = "5678"
+	key = "http://rdf.data-vocabulary.org/rdf.xml"
+	params = {'key':key}
+	contentType = "application/n-triples"
+	header = {'Authorization':"token "+tailrToken, 'Content-Type':contentType}
+	apiURI = "http://localhost:5000/api/user1/repo1"
+	apiURI2 = "http://localhost:5000/api/user2/repo2"
+
+	payload = set()
+
+	def test_put_wrong_token(self):
+		# unauthorized because of wrong token
+		# 401 unauthorized
+		r = requests.put(self.apiURI, params=self.params, headers=self.header, data=self.payload)
+		self.assertEqual(r.status_code, 401)
+
+
+
+
 
 
 # # Create Account with credentials
@@ -105,5 +257,5 @@ class MyTest(unittest.TestCase):
 # # push invalid data and check response
 
 
-# if __name__ == '__main__':
-# 	unittest.main()
+if __name__ == '__main__':
+	unittest.main()
